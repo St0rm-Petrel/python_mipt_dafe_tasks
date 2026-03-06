12dfc426de62549a7c969a1ce00f1cea049dd954
St0rm-Petrel <aleksakhin.viu@phystech.edu>
2026-03-06 16:18:21 +0300

    Solutions for every task

--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -9,11 +9,30 @@ def convert_from_sphere(
     distances: np.ndarray,
     azimuth: np.ndarray,
     inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not distances.shape == azimuth.shape == inclination.shape:
+        raise ShapeMismatchError
+    return tuple(
+        (
+            distances * np.sin(inclination) * np.cos(azimuth),
+            distances * np.sin(inclination) * np.sin(azimuth),
+            distances * np.cos(inclination),
+        )
+    )
 
 
 def convert_to_sphere(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not abscissa.shape == ordinates.shape == applicates.shape:
+        raise ShapeMismatchError
+    distances = np.sqrt(abscissa**2 + ordinates**2 + applicates**2)
+    azimuth = np.arctan2(ordinates, abscissa)
+    inclination = np.arccos(applicates / distances)
+
+    mask = distances == 0
+    inclination[mask] = 0.0
+    azimuth[mask] = 0.0
+    return (distances, azimuth, inclination)
--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,10 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if ordinates.size < 3:
+        raise ValueError
+    mask_max = (ordinates[:-2] < ordinates[1:-1]) & (ordinates[1:-1] > ordinates[2:])
+    mask_min = (ordinates[:-2] > ordinates[1:-1]) & (ordinates[1:-1] < ordinates[2:])
+    ind = np.arange(1, ordinates.size - 1)
+    return tuple((ind[mask_min], ind[mask_max]))
