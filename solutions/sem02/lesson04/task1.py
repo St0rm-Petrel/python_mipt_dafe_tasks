83a1d9ec30ea6f875e09b2005b5c33b7969b5c20
St0rm-Petrel <aleksakhin.viu@phystech.edu>
2026-03-13 20:23:11 +0300

    Solutions for every task

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,66 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    if image.ndim == 2:
+        altitude, width = image.shape
+
+        padd_image = np.zeros((altitude + 2 * pad_size, width + 2 * pad_size), dtype=image.dtype)
+
+        padd_image[pad_size : pad_size + altitude, pad_size : pad_size + width] = image
+
+    elif image.ndim == 3:
+        altitude, width, depth = image.shape
+
+        padd_image = np.zeros(
+            (altitude + 2 * pad_size, width + 2 * pad_size, depth), dtype=image.dtype
+        )
+
+        padd_image[pad_size : pad_size + altitude, pad_size : pad_size + width, :] = image
+
+    return padd_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+
+    if ((kernel_size % 2 == 0) or (kernel_size < 1)):
+        raise ValueError
+
+    if kernel_size == 1:
+        return image
+    
+    i = np.arange(image.shape[0])[:, np.newaxis]
+    j = np.arange(image.shape[1])[np.newaxis, :]
+
+    padd_image = pad_image(image, kernel_size // 2)
+    csum_image_a0 = np.cumsum(padd_image, axis=0)
+    csum_image = np.cumsum(csum_image_a0, axis=1)
+
+    if image.ndim == 2:
+        csum_image_edit = np.zeros((csum_image.shape[0] + 1, csum_image.shape[1] + 1))
+        csum_image_edit[1:, 1:] = csum_image
+
+    else:
+        csum_image_edit = np.zeros(
+            (csum_image.shape[0] + 1, csum_image.shape[1] + 1, csum_image.shape[2])
+        )
+        csum_image_edit[1:, 1:, :] = csum_image
+
+    kernel_sums = (
+        csum_image_edit[i + kernel_size, j + kernel_size]
+        - csum_image_edit[i, j + kernel_size]
+        - csum_image_edit[i + kernel_size, j]
+        + csum_image_edit[i, j]
+    )
+
+    result = kernel_sums / (kernel_size * kernel_size)
+
+    return np.array(result, dtype=np.uint8)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,23 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    pix_unique, count_pixs = np.unique(image, return_counts=True)
+
+    count_all_pix = np.zeros(256)
+    count_all_pix[pix_unique] = count_pixs
+
+    sum_max = 0
+
+    for mid in pix_unique:
+        left = max(0, int(mid) - threshold + 1)
+        r = min(255, int(mid) + threshold - 1)
+        sum_curr = np.sum(count_all_pix[left : r + 1])
+
+        if sum_curr > sum_max:
+            moda = mid
+            sum_max = sum_curr
+
+    return moda, float(sum_max / image.size * 100)
